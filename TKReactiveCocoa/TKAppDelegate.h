7a0eb71bcd11090955129cc9e2e16d73ef949451
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-05-11 18:02:27 +0700

    Initial Commit

---
+++ TKReactiveCocoa/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TKReactiveCocoa
+//
+//  Created by Triệu Khang on 11/5/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TKReactiveCocoa/TKViewController.h
@@ -0,0 +1,13 @@
+//
+//  TKViewController.h
+//  TKReactiveCocoa
+//
+//  Created by Triệu Khang on 11/5/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKViewController : UIViewController
+
+@end
